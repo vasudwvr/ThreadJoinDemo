081998d43794597867234ad92f205cb66c0998b6
vasud <vasud@LAPTOP-O3D1H17F>
2021-04-30 00:14:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Demo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main Thread";
+            Thread t = new Thread(Go);
+            t.Name = "Thread to execute Go nethod";
+            t.Start();
+            Console.WriteLine("This is from Main Thread "+Thread.CurrentThread.Name);
+            t.Join();
+            Console.WriteLine("Thread t has ended!");
+        }
+
+        static void Go()
+        {
+            for (int i = 0; i < 10; i++) Console.WriteLine(Thread.CurrentThread.Name + " y");
+        }
+    }
+}
